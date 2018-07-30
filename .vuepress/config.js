76efaf14c07cc762168adf86c550a1f277d0b102
David Hemphill <davidlee.hemphill@gmail.com>
2018-07-30 00:07:49 -0500

    wip

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,18 @@ module.exports = {
   description: 'Master Your Universe',
   base: '/docs/',
 
+  head: [
+    [
+      'link',
+      {
+        href:
+          'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i',
+        rel: 'stylesheet',
+        type: 'text/css',
+      },
+    ],
+  ],
+
   themeConfig: {
     repo: 'laravel/nova',
     displayAllHeaders: true,
