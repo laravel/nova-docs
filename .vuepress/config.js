0f78a3263b659245ef81df717fad1d4cc8e70906
David Hemphill <davidlee.hemphill@gmail.com>
2018-07-29 23:20:54 -0500

    wip

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,72 +1,23 @@
 module.exports = {
   title: 'Laravel Nova',
   description: 'Master Your Universe',
+  base: '/docs/',
 
   themeConfig: {
     repo: 'laravel/nova',
     displayAllHeaders: true,
-    sidebarDepth: 0,
 
     nav: [
-      { text: 'Home', link: 'https://nova.laravel.com'},
-      { text: 'Version', link: '/',
-        items: [
-          { text: '1.0', link: '/docs/1.0/' },
-        ]
-      }
+      {text: 'Home', link: 'https://nova.laravel.com'},
+      {
+        text: 'Version',
+        link: '/',
+        items: [{text: '1.0', link: '/1.0/'}],
+      },
     ],
 
-    sidebar: [
-      {
-        title: 'Getting Started',
-        collapsable: false,
-        children: [
-          'docs/1.0/getting-started/',
-          'docs/1.0/getting-started/quickstart',
-        ]
-      },
-      {
-        title: 'Dashboard',
-        collapsable: false,
-        children: [
-          'docs/1.0/dashboard/metrics',
-          'docs/1.0/dashboard/tools',
-        ]
-      },
-      {
-        title: 'Resources',
-        collapsable: false,
-        children: [
-          '/docs/1.0/resources/overview',
-          '/docs/1.0/resources/fields',
-          '/docs/1.0/resources/filters',
-          '/docs/1.0/resources/lenses',
-          '/docs/1.0/resources/metrics',
-          '/docs/1.0/resources/single-resource-metrics',
-        ]
-      },
-      {
-        title: 'Customization',
-        collapsable: false,
-        children: [
-          '/docs/1.0/customization/',
-          '/docs/1.0/customization/cards',
-          '/docs/1.0/customization/dashboard-cards',
-          '/docs/1.0/customization/resource-cards',
-          '/docs/1.0/customization/custom-fields',
-          '/docs/1.0/customization/themes',
-        ]
-      },
-      {
-        title: 'Nova JS Reference',
-        collapsable: false,
-        children: [
-          '/docs/1.0/nova-js/',
-          '/docs/1.0/nova-js/methods',
-          '/docs/1.0/nova-js/properties',
-          '/docs/1.0/nova-js/event-bus',
-        ]
-      },
-    ]
-  }
-}
+    sidebar: {
+      '/1.0/': require('./1.0'),
+    },
+  },
+};
