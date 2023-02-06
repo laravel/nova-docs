af0212d71779695394ace53a1d585bb4e9294a91
tal7aouy <tal7aouy.dev@gmail.com>
2023-02-06 14:52:55 +0100

    added:back-to-top

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,40 +5,40 @@ module.exports = {
 
   plugins: [
     [
-      '@vuepress/pwa',
+      "@vuepress/back-to-top",
+      "@vuepress/pwa",
       {
         serviceWorker: true,
-        updatePopup: true
+        updatePopup: true,
       },
     ],
-    require("./plugins/metaVersion.js")
+    require("./plugins/metaVersion.js"),
   ],
 
   head: [
     [
       "link",
       {
-        href:
-          "https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i",
+        href: "https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i",
         rel: "stylesheet",
-        type: "text/css"
-      }
+        type: "text/css",
+      },
     ],
     // Used for PWA
     [
       "link",
       {
         rel: "manifest",
-        href: "/manifest.json"
-      }
+        href: "/manifest.json",
+      },
     ],
     [
       "link",
       {
         rel: "icon",
-        href: "/icon.png"
-      }
-    ]
+        href: "/icon.png",
+      },
+    ],
   ],
 
   themeConfig: {
@@ -58,16 +58,16 @@ module.exports = {
           { text: "1.0", link: "/1.0/" },
           { text: "2.0", link: "/2.0/" },
           { text: "3.0", link: "/3.0/" },
-          { text: "4.0", link: "/4.0/" }
-        ]
-      }
+          { text: "4.0", link: "/4.0/" },
+        ],
+      },
     ],
 
     sidebar: {
       "/1.0/": require("./1.0"),
       "/2.0/": require("./2.0"),
       "/3.0/": require("./3.0"),
-      "/4.0/": require("./4.0")
+      "/4.0/": require("./4.0"),
     },
 
     algolia: {
@@ -75,8 +75,8 @@ module.exports = {
       appId: "FGRCZANQVY",
       apiKey: "7c0aaf326992f08ed7bfc461a1b61ef3",
       algoliaOptions: {
-        facetFilters: ["version:4.0.0"]
-      }
-    }
-  }
+        facetFilters: ["version:4.0.0"],
+      },
+    },
+  },
 };
