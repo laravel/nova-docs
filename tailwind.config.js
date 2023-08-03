0f72fcb9a710569a8efd6450f03e08d1c82c9ca1
Hemp <davidhemphill@users.noreply.github.com>
2023-08-03 11:02:37 -0500

    Vitepress (#546)
    * wip
    
    * wip
    
    * wip

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+import tailwindConfig from '@hempworks/pilgrim/tailwind.config'
+
+const primary = {
+    500: '#0ea5e9',
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    presets: [
+        tailwindConfig,
+    ],
+
+    content: [
+        ...tailwindConfig.content,
+        './.vitepress/theme/**/*.{vue,js,ts,jsx,tsx}',
+        './src/**/*.{md,svg}',
+    ],
+
+    theme: {
+        extend: {
+            colors: { primary },
+            fontFamily: {
+                sans: ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+}
